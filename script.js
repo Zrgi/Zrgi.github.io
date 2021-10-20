e5c47143c4d13c494626f1430350e744cf2771f8
Zrgi <92761066+Zrgi@users.noreply.github.com>
2021-10-20 11:59:04 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-window.alert("peepee");
-
-function name() {
+function userName() {
     var candy = document.getElementById("box").value; //saves the input of top box as [candy]
-    window.alert("test");
     window.alert(candy);
 }
