662e2ca06cfeafc28979798333935da600029505
Zrgi <92761066+Zrgi@users.noreply.github.com>
2021-10-22 12:01:53 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function enterName() {
-    var candy = document.getElementById('box').value;
+var candy;
+
+function getName() {
+    candy = document.getElementById('box').value;
     localStorage.setItem('uName',candy);
     location.href = 'two.html';
 }
 
 function getUser() {
     var b = localStorage.getItem("uName");
-    document.getElementById('para').innerHTML = "Hello " + b;
+    document.getElementById('placeholder').innerHTML = "Hello " + b;
 }
 
