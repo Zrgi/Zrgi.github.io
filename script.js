532dada4129793dbc21ee0aabd75c439cebeb6b6
Zrgi <92761066+Zrgi@users.noreply.github.com>
2021-10-22 11:17:56 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+var candy;
+
 function userName() {
-    var candy = document.getElementById("box").value; //saves the input of top box as [candy]
-    window.alert(candy);
+    candy = document.getElementById("box").value; //saves the input of top box as [candy]
+    localStorage.setItem('uName',candy); //create local storage key with value of [candy]
+    location.href = "two.html"; //automatically take user to page "two.html"
 }
