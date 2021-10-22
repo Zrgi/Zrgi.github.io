ef54219edbc8030b77f546b413f8076089d14224
Zrgi <92761066+Zrgi@users.noreply.github.com>
2021-10-22 11:27:41 -1000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function userName() {
     localStorage.setItem('uName',candy); //create local storage key with value of [candy]
     location.href = "two.html"; //automatically take user to page "two.html"
 }
+
+function getUser() {
+    var b = localStorage.getItem("uName");
+    document.getElementById('box').innerHTML = "Hello " + candy;
+}
+
