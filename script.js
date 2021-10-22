f28c0df7a1fe186aed5b3743b85ba3053770c70a
Zrgi <92761066+Zrgi@users.noreply.github.com>
2021-10-22 11:49:32 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function enterName() {
+    var candy = document.getElementById('box').value;
+    localStorage.setItem('uName','candy');
+    location.href = 'two.html';
+}
+
 function getUser() {
     var b = localStorage.getItem("uName");
     document.getElementById('para').innerHTML = "Hello " + b;
